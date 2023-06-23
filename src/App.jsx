1aadf7ac9b125c80c5c56eb8d50fc725593eda38
gourav222 <gouravparmar775566@gmail.com>
2023-06-23 10:35:56 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
-import AddContact from "./components/AddAndEditContact/AddAndEditContact";
-import SearchContact from "./components/SearchContact";
-import ViewContact from "./components/ViewContact";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Contact />} />
-        <Route path="addContact" element={<AddContact />} />
-        <Route path="searchContact" element={<SearchContact />} />
-        <Route path="viewContact" element={<ViewContact />} />
       </Routes>
     </BrowserRouter>
   );
